6cec25ba6fb278d5f05831f9903e36dbbc147702
CoreySwitzer <cs1177665@swccd.edu>
2025-02-11 21:14:13 +0000

    Created a file

---
+++ EuclideanDivision.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EuclideanDivsion{
+    public static void main (String[] args){
+        //Scanner object to receive keyboard input
+Scanner input = new Scanner(System.in);
+        
+//Prompt the user to enter the height of a rectangle
+System.out.print("Enter an integer: ");
+int num = input.nextInt();
+
+//Display the number
+System.out.println("The number is: " + num);
+    String dividend="dividend"
+    String 
+    }
+}
