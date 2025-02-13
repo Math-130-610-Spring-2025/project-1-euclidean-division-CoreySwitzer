ecbc06d7dbdd39adeb6c6b5d3e27700b2757c212
CoreySwitzer <cs1177665@swccd.edu>
2025-02-13 21:16:23 +0000

    what

--- EuclideanDivision.java
+++ EuclideanDivision.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 public class EuclideanDivsion{
-    public static void main (String[] args){
+    public static void main(String[] args){
         //Scanner object to receive keyboard input
 Scanner input = new Scanner(System.in);
         
 //Prompt the user to enter the height of a rectangle
-System.out.print("Enter an integer: ");
+System.out.println("Enter an integer: ");
 int num = input.nextInt();
 
 //Display the number
 System.out.println("The number is: " + num);
-    String dividend="dividend"
-    String 
+    Int quotient = dividend / divisor;
+    Int remainder = dividend % divisor;
+    Syste.out.println(dividend + " = " + quotient + " * " + divisor + remainder);
     }
 }
